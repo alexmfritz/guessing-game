52fa480663846302d1ce34bd3b849a39f239e423
Alex Fritz <amfritz93@gmail.com>
2026-02-27 09:20:57 -0800

    Create overall game stats text

--- guessing-game/GuessingGame.java
+++ guessing-game/GuessingGame.java
@@ -12,13 +12,23 @@ public class GuessingGame {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
 
-        
+        int totalGames = 0;
+        int totalGuesses = 0;
+        int bestGame = MAX + 1;
     }
 
-    public static void intro() {
+    public static void printIntro() {
         System.out.println("Integer unknown");
         System.out.println("Bound by zero and one hundred");
         System.out.println("Seek it with a guess");
         System.out.println();
+    }
+
+    public static void printStats(int totalGames, int totalGuesses, int bestGame) {
+        System.out.println("Overall results:");
+        System.out.println("Overall games = " + totalGames);
+        System.out.println("Total guesses = " + totalGuesses);
+        System.out.printf("Guesses/Games = %.fn%n", (double) totalGuesses / totalGames);
+        System.out.println("Best game = " + bestGame);
     }
 }
