520202c17855c2b59e2652186fcfbd5250745fd4
Alex Fritz <amfritz93@gmail.com>
2026-02-27 09:06:12 -0800

    Create intro haiku text

--- guessing-game/GuessingGame.java
+++ guessing-game/GuessingGame.java
@@ -1,9 +1,24 @@
+// Alex Fritz
+// Programming Project #3 - Extra Credit
+// CS& 141 - Frank Shang
+
+// Import Java utilities (Scanner and Random classes)
 import java.util.*;
 
 public class GuessingGame {
+    // create game constant for max range of random
     public static final int MAX = 100;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
+
+        
+    }
+
+    public static void intro() {
+        System.out.println("Integer unknown");
+        System.out.println("Bound by zero and one hundred");
+        System.out.println("Seek it with a guess");
+        System.out.println();
     }
 }
